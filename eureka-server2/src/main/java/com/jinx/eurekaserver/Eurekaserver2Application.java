31d7da24c6f3f16a961e48504cc7d35985478be2
liufeng <631819061@qq.com>
2019-04-10 17:31:38 +0800

    1.完成单个注册中心 2.完成多个注册中心的互相注册 3.将服务提供者注册到注册中心

---
+++ eureka-server2/src/main/java/com/jinx/eurekaserver/Eurekaserver2Application.java
@@ -0,0 +1,15 @@
+package com.jinx.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Eurekaserver2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eurekaserver2Application.class, args);
+    }
+
+}
--- springboot-test/src/main/java/com/jinx/springboot/HelloController.java
+++ springboot-test/src/main/java/com/jinx/springboot/HelloController.java
@@ -1,7 +1,12 @@
 package com.jinx.springboot;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author jinx
@@ -9,11 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
  * Desc:
  */
 @RestController
+@Slf4j
 public class HelloController {
 
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping("hello")
     public String index() {
+        ServiceInstance instance = (ServiceInstance) discoveryClient.getInstances("HELLO-SERVICE").get(0);
+        log.info("/hello,host:"+instance.getHost()+",service_id:"+instance.getServiceId());
         return "Hello World";
     }
 
